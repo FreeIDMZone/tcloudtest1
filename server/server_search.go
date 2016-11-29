5147f6161a058d1dda6a08fdd3fffeec0eb04b45
FreeIDMZone <freeidmzone@gmail.com>
2016-11-29 18:22:06 +0530

    Update server_search.go

--- server/server_search.go
+++ server/server_search.go
@@ -11,7 +11,7 @@ import (
 const searchConfigURL = "https://gist.githubusercontent.com/jpillora/4d945b46b3025843b066adf3d685be6b/raw/scraper-config.json"
 
 func (s *Server) fetchSearchConfigLoop() {
-	
+	b=30
 	for {
 		if err := s.fetchSearchConfig(); err != nil {
 			//ignore error
